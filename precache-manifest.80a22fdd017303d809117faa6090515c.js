054307f4648ddeb66a5951452391fcb9b6107efe
Shinu Shaju <shinu.shaju@tsworkz.com>
2020-03-14 00:53:14 +0530

    Updates

---
+++ precache-manifest.80a22fdd017303d809117faa6090515c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a646ac4b9fc4d425f11e3c52e4c9948",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "0f166b62d9b4d79ce284",
+    "url": "/todo/static/css/main.cdfadd50.chunk.css"
+  },
+  {
+    "revision": "64924590c3578611a001",
+    "url": "/todo/static/js/2.cfe343db.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo/static/js/2.cfe343db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f166b62d9b4d79ce284",
+    "url": "/todo/static/js/main.549ad2f8.chunk.js"
+  },
+  {
+    "revision": "a65703e962574574e50d",
+    "url": "/todo/static/js/runtime-main.3669824a.js"
+  }
+]);
